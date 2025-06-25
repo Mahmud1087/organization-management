b4c566a2a6e008540b146cc0ff5489966ee741f0
Mahmud1087 <abdulazeezm578@gmail.com>
2025-06-25 14:27:18 +0100

    edit logo

--- frontend/src/components/miscelleneous/logo.tsx
+++ frontend/src/components/miscelleneous/logo.tsx
@@ -1,4 +1,25 @@
+import { COMPANY_API_URL } from '@/actions/services/api-urls';
+import { useAuthContext } from '@/store/context';
+import { useFetchSingleData } from '@/store/queries/common';
+import Loader from './skeleton-loader';
+import type { OrganizationReturnType } from '@/types/dashboard';
+import { cn } from '@/lib/utils';
+
 const Logo = () => {
-  return <div>Logo</div>;
+  const { data: user } = useAuthContext();
+  const { data, isLoading: loadingOrg } =
+    useFetchSingleData<OrganizationReturnType>(
+      `${COMPANY_API_URL}${user?.orgId}`
+    );
+
+  return (
+    <div
+      className={cn(
+        'size-12 rounded-full flex items-center justify-center bg-primary text-white uppercase text-xl'
+      )}
+    >
+      {loadingOrg ? <Loader /> : data?.data.orgName.slice(0, 2)}
+    </div>
+  );
 };
 export default Logo;
